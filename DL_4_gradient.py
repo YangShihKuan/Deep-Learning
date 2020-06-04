5878bf36325bc983bb17f2ac8eaf23c057af80c8
YangShihKuan <shihkuanyang1274@gmail.com>
2020-06-04 16:04:14 +0800

    gradient start

---
+++ DL_4_gradient.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 16:01:05 2020
+
+@author: user
+"""
+
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numerical_gradient
+
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
