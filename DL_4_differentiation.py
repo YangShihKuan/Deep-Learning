42191ce0e2a1728278fabb6b188ad5a0915bbb11
YangShihKuan <shihkuanyang1274@gmail.com>
2020-06-04 16:02:59 +0800

    differ

---
+++ DL_4_differentiation.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 15:19:04 2020
+
+@author: user
+"""
+
+import numpy as np
+import matplotlib.pylab as plt
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x+h) - f(x-h))/(2*h)
+
+def function_1(x):
+    return 0.01*x**2 + 0.1*x
+
+def tangent_line(f, x):
+    d = numerical_diff(f, x)
+    print(d)
+    y = f(x) - d*x
+    return lambda t: d*t + y
+
+x = np.arange(0.0, 20.0, 0.1)
+y = function_1(x)
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.plot(x, y)
+#plt.show
+
+
+tf = tangent_line(function_1, 5)
+print(tf)
+y2 = tf(x)
+
+plt.plot(x, y2)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
