69826232e00dc73b21f159a8c48e86127dd041b3
YangShihKuan <shihkuanyang1274@gmail.com>
2020-06-04 16:14:54 +0800

    gradient over

--- DL_4_gradient.py
+++ DL_4_gradient.py
@@ -11,7 +11,7 @@ import numpy as np
 from common.functions import softmax, cross_entropy_error
 from common.gradient import numerical_gradient
 
-
+np.random.seed(1)
 class simpleNet:
     def __init__(self):
         self.W = np.random.randn(2,3)
@@ -25,3 +25,45 @@ class simpleNet:
         loss = cross_entropy_error(y, t)
 
         return loss
+
+net = simpleNet()
+print(net.W)
+x = np.array([0.6, 0.9])
+p = net.predict(x)
+print(p)
+
+t = np.array([0, 0, 1])
+net.loss(x,t)
+
+f = lambda w: net.loss(x, t)
+dW = numerical_gradient(f, net.W)
+
+print(dW)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
